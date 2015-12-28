f8ac78d0522ec7801527f70869d7c218bc3924a3
Helen Miles <lionspaws@hotmail.com>
2015-12-28 17:28:37 +0000

    refactoring to place product in a new js file

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,6 +1,6 @@
 (function() {
     
-    var app = angular.module('gemStore',[]);
+    var app = angular.module('gemStore',['gemStore-products']);
     
     app.controller('StoreController', function() {
         this.products = gems;
@@ -38,73 +38,10 @@
         };
     });
     
-    app.directive('productTitle', function(){
-        return {
-            restrict: 'E',  // element
-            templateUrl: 'product-title.html'
-        };
-    });
-    
     app.directive('storeTitle', function(){
         return {
             restrict: 'A',  // attribute
             templateUrl: 'store-title.html'
-        };
-    });
-    
-    app.directive('productPanels', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'product-panels.html',
-            /*
-            controller: function(){
-                this.panel = 1;
-
-                this.selectPanel = function(setPanel) {
-                    this.panel = setPanel;
-                };
-
-                this.isSelected = function(checkPanel) {
-                    return this.panel === checkPanel;
-                };
-            },
-            controllerAs: "panel"
-            */
-        };
-    });
-    
-    app.directive('productGallery', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function(){
-                this.current = 0;
-                this.setCurrent = function(imageNumber){
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
-    });
-    
-    app.directive('productDescription', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-description.html'
-        };
-    });
-
-    app.directive('productReviews', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-reviews.html'
-        };
-    });
-
-    app.directive('productSpecification', function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-specification.html"
         };
     });
     
